e6aa3543b53c2fe2fd92f21a896fb047471b29c5
mwgeneral <maxwell.kingwilson@gmail.com>
2015-07-11 12:08:22 -0400

    Set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MetaphorDatabase::Application.routes.draw do
+  root "pages#home"
+  get "pages/home"
+  get "pages/about"
+  get "pages/login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
