2bf8a5844d70a3a401a319eed7dfd28da2085be4
mwgeneral <maxwell.kingwilson@gmail.com>
2015-07-10 20:21:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MetaphorDatabase::Application.config.secret_key_base = '7ae7727bc73b84e5a3df80783bed6a8f5c5c64c55582f4ffadab9294288177827906431525b436f1de90b165ead3307cfa77d956d9080be8d5a642c9bf65aca7'
