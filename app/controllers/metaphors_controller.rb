d821402c968a8a63e07563a3cbf658e8050e006e
mwgeneral <maxwell.kingwilson@gmail.com>
2015-07-24 21:05:49 -0400

    test model insertion

---
+++ app/controllers/metaphors_controller.rb
@@ -0,0 +1,2 @@
+class MetaphorsController < ApplicationController
+end
---
+++ test/controllers/metaphors_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MetaphorsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
